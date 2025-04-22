9f9a6e04c7e8f34daef7e18d2d3a1e846e014ade
Yahia Ghadiry <yahiaghadiry@gmail.com>
2025-04-22 14:39:33 +0200

    UART Initalization function implmentation

---
+++ bit_utilies.h
@@ -0,0 +1,17 @@
+#ifndef Bit_Utilies
+#define Bit_Utilies
+
+#define GET_BIT(reg1,bit)   (((reg1)>>(bit))&1)
+#define GET_REG(reg)        ((reg)&0xff)
+
+#define SET(reg,val)        ((reg) |= (val))
+#define SET_BIT(reg,bit)    ((reg) |= (1<<(bit)))
+
+#define CLR(reg,val)        ((reg) &=~(val))
+#define CLR_BIT(reg,bit)    ((reg) &=~(1<<(bit)))
+
+#define MASK(reg, mask) ((reg) & (mask))
+
+#define REV_MASK(reg, mask) ((reg) & ~(mask))
+
+#endif
---
+++ uart.d
@@ -0,0 +1,5 @@
+.\uart.o: uart.c
+.\uart.o: uart.h
+.\uart.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\uart.o: TM4C123.h
+.\uart.o: bit_utilies.h
