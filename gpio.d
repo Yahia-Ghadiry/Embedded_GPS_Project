c769cc7f07b06f6b944b8e349dfc243ce95c872d
Yahia Mohammed <yahiamedo5@gmail.com>
2025-04-27 22:30:55 +0300

    fhhjj

---
+++ gpio.d
@@ -0,0 +1,3 @@
+.\gpio.o: GPIO.c
+.\gpio.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\gpio.o: tm4c123.h
--- uart.d
+++ uart.d
@@ -2,4 +2,3 @@
 .\uart.o: uart.h
 .\uart.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
 .\uart.o: TM4C123.h
-.\uart.o: bit_utilies.h
